eeef55dff21dce006eae301b2e206a0b6536ebd3
Cyber Phantom <inam123451@gmail.com>
2024-11-28 13:38:59 +0530

    Parse arguments

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
+use clap::Parser;
+use std::path::PathBuf;
+
+#[derive(Parser, Debug)]
+#[command(
+    author = "Inam Ul Haq",
+    version = "1.0",
+    about = "Android Firmware Extractor"
+)]
+struct Arguments {
+    firmware_zip_path: PathBuf,
+
+    #[arg(short = 'o', long = "output")]
+    output: Option<PathBuf>,
+
+    #[arg(short = 'p', long = "partitions", value_delimiter = ',')]
+    partitions: Vec<String>,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args: Arguments = Arguments::parse();
 }
