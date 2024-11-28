7d452e3cf6b951f328829d702ffb45971a91ae9f
Cyber Phantom <inam123451@gmail.com>
2024-11-28 18:22:32 +0530

    utils: Add funcion to generate default path

--- src/utils.rs
+++ src/utils.rs
@@ -1,6 +1,6 @@
 use std::fs::File;
 use std::os::unix::fs::FileExt;
-use std::path::Path;
+use std::path::{Path, PathBuf};
 pub struct ZipFile(File);
 
 const ZIP_SIGNATURE: [u8; 4] = [0x50, 0x4B, 0x03, 0x04];
@@ -33,4 +33,13 @@ impl ZipFile {
             .map(|file| file.split('/').last().unwrap().to_string())
             .collect()
     }
+}
+
+pub fn default_output_path(firmware_zip_path: &Path) -> PathBuf {
+    let base_dir = firmware_zip_path.parent().unwrap();
+    let time_now = std::time::SystemTime::now()
+        .duration_since(std::time::UNIX_EPOCH)
+        .unwrap();
+
+    base_dir.join(format!("output-{}", time_now.as_secs()))
 }
